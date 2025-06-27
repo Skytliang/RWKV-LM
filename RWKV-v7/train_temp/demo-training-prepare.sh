271d5b68af3fa5db7b7dfedd6138b40a808860f4
ttianliang <ttianliang@tencent.com>
2025-06-27 17:46:28 +0800

    demo-training-run

--- RWKV-v7/train_temp/demo-training-prepare.sh
+++ RWKV-v7/train_temp/demo-training-prepare.sh
@@ -7,8 +7,8 @@
 #
 # Please firstly create data folder & Download minipile (1498226207 tokens, around 3GB)
 # mkdir -p data
-# wget --continue -O data/minipile.idx https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.idx
-# wget --continue -O data/minipile.bin https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.bin
+# wget --continue -O data/minipile/minipile.idx https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.idx
+# wget --continue -O data/minipile/minipile.bin https://huggingface.co/datasets/BlinkDL/minipile-tokenized/resolve/main/rwkv_vocab_v20230424/minipile.bin
 #
 #######################################################################################################################
 #
